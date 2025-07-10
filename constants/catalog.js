a12855f7b5afc309868216854c31df91547ff8c2
OldPole <ipetr6828@gmail.com>
2025-07-11 01:17:35 +0300

    created structure of the project and added data for the catalog

---
+++ constants/catalog.js
@@ -0,0 +1,26 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'Мега Охотничья',
+        img: 'img/catalog/pizza1.png',
+        price: 13.90,
+    },
+        {
+        id: 'el2',
+        name: 'Четыре сыра',
+        img: 'img/catalog/pizza2.png',
+        price: 24.40,
+    },
+        {
+        id: 'el3',
+        name: 'Пепперони',
+        img: 'img/catalog/pizza3.png',
+        price: 18.40,
+    },
+        {
+        id: 'el4',
+        name: 'Ветчина и грибы',
+        img: 'img/catalog/pizza4.png',
+        price: 18.40,
+    },
+];
