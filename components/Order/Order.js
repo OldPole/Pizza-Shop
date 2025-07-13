24fe00685f06e6d07f2ff3e0932ac449a5c0ff81
OldPole <ipetr6828@gmail.com>
2025-07-13 13:50:20 +0300

    remaked component order and markup for it

--- components/Order/Order.js
+++ components/Order/Order.js
@@ -1,8 +1,8 @@
 class Order {
 
     constructor() {
-        this.keyRadius = '25 см';
-        this.keyBase = 'традиционное';
+        this.keySize = '25 см';
+        this.keyBase = 'Традиционное';
     }
 
     handleClear() {
@@ -12,8 +12,8 @@ class Order {
     render(id) {
         const product = CATALOG.find(element => element.id === id);
         const index =
-            this.keyRadius === '25 см' ? 0 : 
-            this.keyRadius === '30 см' ? 1 : 2;
+            this.keySize === '25 см' ? 0 : 
+            this.keySize === '30 см' ? 1 : 2;
         const html = `
         <div class="order-overlay">
             <div class="order-container">
@@ -21,33 +21,18 @@ class Order {
                 <img class="order__img" src=${product.img}></img>
                 <div class="order__content">
                     <h3 class="order__name">${product.name}</h3>
-                    <span class="order__info">${this.keyRadius + ', ' + product.weights[index] + ' г' + ', ' + this.keyBase + ' тесто'}</span>
+                    <span class="order__info">${this.keySize + ', ' + product.weights[index] + ' г' + ', ' + this.keyBase + ' тесто'}</span>
                     <p class="order__ingredients">${product.ingredients}</p>
-                    <form>
-                        <label>
-                            <input type="radio" name="pizza-size" value="25 см" ${this.keyRadius === '25 см' ? 'checked' : ''}>
-                            25 см
-                        </lable>
-                        <label>
-                            <input type="radio" name="pizza-size" value="30 см" ${this.keyRadius === '30 см' ? 'checked' : ''}>
-                            30 см
-                        </lable>
-                        <label>
-                            <input type="radio" name="pizza-size"  value="35 см"${this.keyRadius === '35 см' ? 'checked' : ''}>
-                            35 см
-                        </lable>
-                    </form>
-                    <form>
-                        <lable>
-                            <input type="radio" name="pizza-base" value="традиционное" ${this.keyBase === 'традиционное' ? 'checked' : ''}>
-                            традиционное
-                        </lable>
-                        <lable>
-                            <input type="radio" name="pizza-base" value="тонкое" ${this.keyBase === 'тонкое' ? 'checked' : ''}>
-                            тонкое
-                        </lable>
-                    </form>
-                    <button class="order__btn" onclick="">В корзину за ${product.prices[index] + ' BYN'}</button>
+                    <div class="order__options">
+                        <button class="size-option ${this.keySize === '25 см' ? 'size-optin_active' : ''}" data-size="25 см">25 см</button>
+                        <button class="size-option ${this.keySize === '30 см' ? 'size-optin_active' : ''}" data-size="30 см">30 см</button>
+                        <button class="size-option ${this.keySize === '35 см' ? 'size-optin_active' : ''}" data-size="35 см">35 см</button>
+                    </div>
+                    <div class="order__options">
+                        <button class="base-option ${this.keyBase === 'Традиционное' ? 'base-option_active' : ''}" data-base="Традиционное">Традиционное</button>
+                        <button class="base-option ${this.keyBase === 'Тонкое' ? 'base-option_active' : ''}" data-base="Тонкое">Тонкое</button>
+                    </div>
+                    <button class="order__btn" onclick="orderPage.handleClear();">В корзину за ${product.prices[index] + ' BYN'}</button>
                 </div>
             </div>
         </div>
@@ -55,19 +40,27 @@ class Order {
 
         ROOT_ORDER.innerHTML = html;
 
-        document.querySelectorAll('input[name="pizza-size"]').forEach(element => {
-            element.addEventListener('change', event => {
-                this.keyRadius = event.target.value;
-                this.render(id);
+        document.querySelectorAll('.size-option').forEach(element => {
+            element.addEventListener('click', event => {
+                const newSize = event.target.dataset.size;
+
+                if ( newSize !== this.keySize) {
+                    this.keySize = newSize;
+                    this.render(id);
+                }
             })
         });
 
-        document.querySelectorAll('input[name="pizza-base"]').forEach(element => {
-            element.addEventListener('change', event => {
-                this.keyBase = event.target.value;
-                this.render(id);
+        document.querySelectorAll('.base-option').forEach(element => {
+            element.addEventListener('click', event => {
+                const newBase = event.target.dataset.base;
+
+                if (newBase !== this.keyBase) {
+                    this.keyBase = newBase;
+                    this.render(id);
+                }
             })
-        })
+        });
     }
 }
 
